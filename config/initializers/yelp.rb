585bed143d72bb47725c99594e843014f77fcc2f
Rutger Geelen <rutgergeelen@schedjoules.com>
2016-08-02 22:08:37 +0200

    Add map

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,8 +1,8 @@
 require 'yelp'
 
 Yelp.client.configure do |config|
-  config.consumer_key = 'ZHPjUbs5xOW7dxC0uULMTw'
-  config.consumer_secret = '9bC_mhki5gKRWmevKFVyv6CCnuI'
-  config.token = 'RxK-JNMFobE3b6rcj0O3kYvqXOL26ArJ'
-  config.token_secret = '-HPB1hbCDIAs5lvdM1LnPE5D1R'
+  config.consumer_key = Rails.application.secrets.yelp_consumer_key
+  config.consumer_secret = Rails.application.secrets.yelp_consumer_secret
+  config.token = Rails.application.secrets.yelp_consumer_secret
+  config.token_secret = Rails.application.secrets.yelp_consumer_secret
 end
